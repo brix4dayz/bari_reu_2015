fe0b535a81669f30cf80dff7792e3f87ec52211a
W Hayden Fuss <wifu1234@gmail.com>
2015-06-10 10:44:37 -0400

    Initial commit. Made inidividual directories and added some prototype scripts.

---
+++ hayden/analyze_tweets.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python2.7
+
+keywords = ["#bostonstrong", "#prayforboston", "police", "cops", "shooting", "bombing", "marathon"]
+
+def tweetContainsKeyWords(tweet):
+  for kw in keywords:
+    if kw in tweet: return True
+
+def angryTweet(tweet):
+  return
+
+def peacefulTweet(tweet):
+  return
+
+def scaredTweet(tweet):
+  return
+
+# source: https://docs.python.org/2/library/csv.html
+# uses the csv.DictReader class to parse the data
+# because the first line of the file explains each field in a csv
+# format, DictReader immediately knows how to parse each line into
+# a hash/dictionary with the keys matching the fields specified in 
+# the first line of the file
+
+numTweetsWithKeywords = 0
+
+import csv
+with open('cleaned_geo_tweets_Apr_12_to_22.csv') as csvfile:
+  tweets = csv.DictReader(csvfile)
+  i = 0
+  # for all the tweets the reader finds
+  for tweetData in tweets:
+    print "Tweet #" + str(i)
+    i = i + 1
+    print tweetData['time'] 
+    print tweetData['tweet_text'] + "\n"
+    if tweetContainsKeyWords(tweetData['tweet_text'].lower()):
+      numTweetsWithKeywords = numTweetsWithKeywords + 1
+
+print numTweetsWithKeywords
+print float(numTweetsWithKeywords)/i
---
+++ hayden/first_ten_tweets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python2.7
+
+"""
+  Prints the first 10 tweets as raw data.
+"""
+
+import csv
+
+# source for printing first n rows: 
+# http://stackoverflow.com/questions/7661540/print-the-first-two-rows-of-a-csv-file-to-a-standard-output
+
+n = 10
+
+with open('cleaned_geo_tweets_Apr_12_to_22.csv') as csvfile:
+  tweets = csv.DictReader(csvfile)
+  for i in range(n):
+    print "Tweet #" + str(i)
+    tweetData = tweets.next() # gets the next hash from the reader
+    print str(tweetData) + "\n"
