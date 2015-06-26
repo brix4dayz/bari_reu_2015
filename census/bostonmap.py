4b6a8232d4d0ee8ae689132bba3feb81d28d4162
W Hayden Fuss <wifu1234@gmail.com>
2015-06-26 17:36:59 -0400

    Added a little more to bostonmap, still very incomplete.

--- census/bostonmap.py
+++ census/bostonmap.py
@@ -1,7 +1,21 @@
+import csv
+import yaml
+import math
+import re
 
 grid = None
 
 cutoff = 1.0
+
+bostonXMIN = 0.0
+
+bostonXMAX = 0.0
+
+bostonYMIN = 0.0
+
+bostonYMAX = 0.0
+
+binsize = 10.0
 
 def initGrid():
   return 
@@ -15,7 +29,7 @@ def test():
 def distance(parcel, datum):
   return 
 
-class BinGrid(object):
+class ParcelGrid(object):
   def __init__(self, xmin, xmax, ymin, ymax, binsize):
     self.grid = {}
     for i in range(xmin, xmax, binsize):
@@ -24,13 +38,21 @@ class BinGrid(object):
         self.grid[i][j] = []
     return
 
-  def add(self, datum):
+  def addParcel(self, parcel):
+    i = int(float(parcel['X']))/binsize
+    j = int(float(parcel['Y']))/binsize
+    self.grid[i][j].append(parcel)
+    return
+
+  def addData(self, datum):
     global cutoff
     i = int(float(datum['lon']))/binsize
-    j = int(float(datum['lon']))/binsize
+    j = int(float(datum['lat']))/binsize
     for p in self.grid[i][j]:
       if distance(p, datum) <= cutoff:
         p.append(datum)
+        return
+
 
 if __name__ == "__main__":
   test()
