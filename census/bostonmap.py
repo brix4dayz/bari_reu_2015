9c9c01c0975bbfdf9a500b48e490f62ff7e10bae
W Hayden Fuss <wifu1234@gmail.com>
2015-06-30 12:09:30 -0400

    Finished BostonMap and BostonScatter.

--- census/bostonmap.py
+++ census/bostonmap.py
@@ -1,5 +1,3 @@
-import csv
-import yaml
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import numpy as np
@@ -9,13 +7,11 @@ from itertools import chain
 from matplotlib.collections import PatchCollection
 from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
 from descartes import PolygonPatch
-import geopandas as gp
 
 #source: http://sensitivecities.com/so-youd-like-to-make-a-map-using-python-EN.html#.VZGG_VyZ65Y
 
 class BostonMap(object):
-  def __init__(self, outname):
-    self.outname = outname
+  def __init__(self):
     shp = fiona.open('Tracts_Boston_2010_BARI.shp')
     bds = shp.bounds
     shp.close()
@@ -29,20 +25,18 @@ class BostonMap(object):
   def loadBasemap(self):
     self.fig = plt.figure()
     self.ax = self.fig.add_subplot(111)
-
     self.map = Basemap(
-    projection='tmerc',
-    lon_0=(self.coords[0] + self.coords[2])/2, # here was where I went wrong
-    lat_0=(self.coords[3] + self.coords[1])/2,
-    llcrnrlon=self.coords[0] - self.extra * self.w,
-    llcrnrlat=self.coords[1] - self.extra + 0.01 * self.h,
-    urcrnrlon=self.coords[2] + self.extra * self.w,
-    urcrnrlat=self.coords[3] + self.extra + 0.01 * self.h,
-    lat_ts=0,
-    resolution='h',
-    area_thresh=0.05,
-    suppress_ticks=True)
-
+      projection='tmerc',
+      lon_0=(self.coords[0] + self.coords[2])/2, # here was where I went wrong
+      lat_0=(self.coords[3] + self.coords[1])/2,
+      llcrnrlon=self.coords[0] - self.extra * self.w,
+      llcrnrlat=self.coords[1] - self.extra + 0.01 * self.h,
+      urcrnrlon=self.coords[2] + self.extra * self.w,
+      urcrnrlat=self.coords[3] + self.extra + 0.01 * self.h,
+      lat_ts=0,
+      resolution='h',
+      area_thresh=0.01,
+      suppress_ticks=True)
     self.map.readshapefile(
       'Tracts_Boston_2010_BARI',
       'boston',
@@ -50,13 +44,12 @@ class BostonMap(object):
       zorder=2)
     return
 
-  def buildDFs(self):
+  def mapDF(self):
     # set up a map dataframe
     self.df_map = pd.DataFrame({
       'poly': [Polygon(xy) for xy in self.map.boston]})
     self.df_map['area_m'] = self.df_map['poly'].map(lambda x: x.area)
     self.df_map['area_km'] = self.df_map['area_m'] / 100000
-
     # draw ward patches from polygons
     self.df_map['patches'] = self.df_map['poly'].map(lambda x: PolygonPatch(
       x,
@@ -65,15 +58,22 @@ class BostonMap(object):
       zorder=4))
     return
 
-  def plotMap(self):
-    self.loadBasemap()
+  # virtual function to be overridden
+  def dataDF(self):
+    return
 
-    self.buildDFs()
+  def buildDFs(self):
+    self.mapDF()
+    self.dataDF()
+    return
 
+  def leverettG(self):
     # plot leverett g tower for fun
     x,y = self.map(-71.1161, 42.3692)
     self.map.plot(x, y, 'go', markersize=5)
+    return
 
+  def mapScale(self):
     # Draw a map scale
     self.map.drawmapscale(
       self.coords[0] + self.w/4, self.coords[3],
@@ -84,18 +84,79 @@ class BostonMap(object):
       fontcolor='#000055',
       zorder=5,
       units='mi')
-
-    # plot boroughs by adding the PatchCollection to the axes instance
-    self.ax.add_collection(PatchCollection(self.df_map['patches'].values, match_original=True))
-
-    plt.title("Boston")
-    # this will set the image width to 722px at 100dpi
-    #fig.set_size_inches(7.22, 5.25)
-    plt.savefig(self.outname + '.png', dpi=100, alpha=True)
-    plt.show()
     return
 
-if __name__ == "__main__":
+  def tracts(self):
+    # plot tracts by adding the PatchCollection to the axes instance
+    self.ax.add_collection(PatchCollection(self.df_map['patches'].values, match_original=True))
+    return
+
+  # virtual function to be overridden
+  def data(self):
+    self.leverettG()
+    return
+
+  def makePlot(self, outname, title):
+    plt.title(title)
+    #self.fig.set_figsize(...)
+    plt.savefig(outname + '.png', dpi=100, alpha=True)
+    return
+
+  def plotMap(self, outname='boston', title='Boston'):
+    self.loadBasemap()
+    self.buildDFs()
+    self.mapScale()
+    self.tracts()
+    self.data()
+    self.makePlot(outname, title)
+    return
+
+class BostonScatter(BostonMap):
+  def __init__(self, dataPoints):
+    self.dataPoints = dataPoints
+    # call parent constructor
+    super(BostonScatter, self).__init__()
+
+  def dataDF(self):
+    output = {'lon':[], 'lat':[]}
+    for d in self.dataPoints:
+      for each in ('lon', 'lat'):
+        output[each].append(d[each])
+
+    df = pd.DataFrame(output)
+    df[['lon', 'lat']] = df[['lon', 'lat']].astype(float)
+
+    self.dataPoints = pd.Series(
+      [Point(self.map(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(df['lon'], df['lat'])])
+    return
+
+  def data(self):
+    self.map.scatter(
+      [geom.x for geom in self.dataPoints],
+      [geom.y for geom in self.dataPoints],
+      10, marker='o', lw=.25,
+      facecolor='#33ccff', edgecolor='w',
+      alpha=0.9, antialiased=True, zorder=3)
+
+    #self.map.drawcoastlines()
+    return
+
+def testMap():
   out = raw_input("Enter name of output png: ")
-  boston = BostonMap(out)
-  boston.plotMap()
+  boston = BostonMap()
+  boston.plotMap(outname=out)
+
+def testScatter():
+  out = raw_input("Enter name of output png: ")
+  testData = [
+    {'lon':'-71.0120751', 'lat':'42.27090738'},
+    {'lon':'-71.1864942', 'lat':'42.36610796'},
+    {'lon':'-71.0530936', 'lat':'42.35931187'}
+  ]
+  boston = BostonScatter(testData)
+  boston.plotMap(outname=out, title='Scatterplot Over Boston')
+  return
+
+if __name__ == "__main__":
+  testScatter()
+  plt.show()
