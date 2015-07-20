3579fae82305a8631f7188e1ceda096841c4ba8a
W Hayden Fuss <wifu1234@gmail.com>
2015-07-20 14:57:59 -0400

    Edited trainer.

--- tweets/train_sentiment.py
+++ tweets/train_sentiment.py
@@ -4,12 +4,12 @@ name = raw_input("Enter your name (hayden, jeremy, liz): ")
 name = name.lower()
 
 fearful = []
-cheerful = []
+excited = []
 angry = []
 sad = []
-objective = []
-peaceful = []
-undetermined = []
+positive = []
+calm = []
+negative = []
 
 with open(name + "_training.txt", "r") as inFile:
   for tweet in inFile:
@@ -17,10 +17,10 @@ with open(name + "_training.txt", "r") as inFile:
     print tweet
     sentiment = ""
     while (sentiment == ""):
-      sentiment = raw_input("Enter the tweets sentiment: (c-heerful, a-angry, f-earful, s-ad, p-eaceful, o-bjective, u-ndetermined, or n-one (not English)): ")
+      sentiment = raw_input("Enter the tweets sentiment: (c-alm, e-xcited, a-angry, f-earful, s-ad, p-ositive, n-egative, or o-ther (not English)): ")
     sentiment = sentiment.lower()[0]
     if sentiment == 'c':
-      cheerful.append(sentiment)
+      calm.append(sentiment)
     elif sentiment == 'a':
       angry.append(sentiment)
     elif sentiment == 'f':
@@ -28,35 +28,35 @@ with open(name + "_training.txt", "r") as inFile:
     elif sentiment == 's':
       sad.append(sentiment)
     elif sentiment == 'p':
-      peaceful.append(sentiment)
-    elif sentiment =='u':
-      undetermined.append(sentiment)
-    elif sentiment == 'o':
-      objective.append(sentiment)
+      positive.append(sentiment)
+    elif sentiment =='n':
+      negative.append(sentiment)
+    elif sentiment == 'e':
+      excited.append(sentiment)
 
 with open(name + "_fearful.txt", "w") as out:
   out.write('\n'.join(fearful))
 
 
-with open(name + "_peaceful.txt", "w") as out:
-  out.write('\n'.join(peaceful))
+with open(name + "_calm.txt", "w") as out:
+  out.write('\n'.join(calm))
 
 
 with open(name + "_angry.txt", "w") as out:
   out.write('\n'.join(angry))
 
 
-with open(name + "_cheerful.txt", "w") as out:
-  out.write('\n'.join(cheerful))
+with open(name + "_excited.txt", "w") as out:
+  out.write('\n'.join(excited))
 
 
 with open(name + "_sad.txt", "w") as out:
   out.write('\n'.join(sad))
 
 
-with open(name + "_objective.txt", "w") as out:
-  out.write('\n'.join(objective))
+with open(name + "_positive.txt", "w") as out:
+  out.write('\n'.join(positive))
 
 
-with open(name + "_undetermined.txt", "w") as out:
-  out.write('\n'.join(undetermined))
+with open(name + "_negative.txt", "w") as out:
+  out.write('\n'.join(negative))
