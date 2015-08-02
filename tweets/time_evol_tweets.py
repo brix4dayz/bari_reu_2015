f3b290b3e6131be4012b1d057aa195d6054cd45b
W Hayden Fuss <wifu1234@gmail.com>
2015-08-02 11:30:43 -0400

    Made info tracking maps for keywords.

--- tweets/time_evol_tweets.py
+++ tweets/time_evol_tweets.py
@@ -12,16 +12,47 @@ import os
 import sys
 sys.path.append(os.path.realpath('../census/'))
 import bostonmap as bm
-sys.path.append(os.path.realpath('./classifiers/'))
-import tweetclassifier as tc
+# sys.path.append(os.path.realpath('./classifiers/'))
+# import tweetclassifier as tc
+import re
 
-paths = {'sad':'/sadTraining.txt', 'fearful':'/fearfulTraining.txt', 'angry':'/angryTraining.txt', 'calm':'/calmTraining.txt',
- 'excited':'/excitedTraining.txt', 'other':'/otherTraining.txt'}
+# paths = {'sad':'/sadTraining.txt', 'fearful':'/fearfulTraining.txt', 'angry':'/angryTraining.txt', 'calm':'/calmTraining.txt',
+#  'excited':'/excitedTraining.txt', 'other':'/otherTraining.txt'}
 
-cats = paths.keys()
+# cats = paths.keys()
 
-clssfr = tc.TweetClassifierLinearSVM(paths, twc.cleanForSentiment)
+# clssfr = tc.TweetClassifierLinearSVM(paths, twc.cleanForSentiment)
 
+handles = ['@boston_police',
+           '@bostonglobe',
+           '@bostonmarathon',
+           '@redsox',
+           '@barackobama',
+           '@bostondotcom',
+           '@stoolpresidente',
+           '@nhlbruins',
+           '@mlb',
+           '@cnnbrk',
+           '@ap',
+           '@7news',
+           '@fox25news',
+           '@youranonnews',
+           '@cnn',
+           '@middlebrooks',
+           '@dzhokhar_a',
+           '@wcvb',
+           '@cbsboston',
+           '@universalhub',
+           '@tdgarden',
+           '@massstatepolice',
+           '@j_tsar',
+           '@huffingtonpost',
+           '@wbcsays']
+
+handle_reg = re.compile('|'.join(handles))
+
+def containsHandle(tweet):
+    return handle_reg.search(tweet) is not None
 
 def getTimeString(currentHour):
     timeStr = ""
@@ -44,14 +75,16 @@ tweet_time_fmt = twc.getTwitterTimeFmt()
 kwTweets = []
 currentHour = 14
 
-sentimentTweets = {}
-for c in cats:
-    sentimentTweets[c] = []
+infoTweets = []
 
-tweetList = []
-textList = []
+# sentimentTweets = {}
+# for c in cats:
+#     sentimentTweets[c] = []
 
-with open('recleaned_geo_tweets_12-22.csv') as csvfile:
+# tweetList = []
+# textList = []
+
+with open('cleaned_geo_tweets_4_12_22.csv') as csvfile:
     # reads first line of csv to determine keys for the tweet hash, tweets 
     # is an iterator through the list of tweet hashes the DictReader makes
     tweets = csv.DictReader(csvfile)
@@ -61,55 +94,68 @@ with open('recleaned_geo_tweets_12-22.csv') as csvfile:
         if tweetData['time'] != "":
             # parse date/time into object
             date = time.strptime(tweetData['time'], tweet_time_fmt)
-            #if date.tm_mday == 15 and twc.tweetContainsKeyword(tweetData['tweet_text']):
-            if date.tm_mday == 15:
+            if date.tm_mday == 15 and twc.tweetContainsKeyword(tweetData['tweet_text']):
+            #if date.tm_mday == 15:
                 if date.tm_hour == currentHour:
-                    #kwTweets.append(tweetData)
-                    tweetList.append(tweetData)
-                    textList.append(tweetData['tweet_text'])
+                    kwTweets.append(tweetData)
+                    #tweetList.append(tweetData)
+                    #textList.append(tweetData['tweet_text'])
+                    if containsHandle(tweetData['tweet_text']):
+                        infoTweets.append(tweetData)
                 elif date.tm_hour == currentHour + 1:
                     currentHour += 1
                     timeStr = getTimeString(currentHour)
-                    results = clssfr.classify(textList)
-                    for i in range(0, len(results)):
-                        sentimentTweets[cats[results[i]]].append(tweetList[i])
+                    # results = clssfr.classify(textList)
+                    # for i in range(0, len(results)):
+                    #     sentimentTweets[cats[results[i]]].append(tweetList[i])
 
-                    for sentiment in sentimentTweets.keys():
-                        boston = bm.GreaterBostonScatter(sentimentTweets[sentiment])
-                        boston.plotMap(outname=sentiment + '_bombing_scatter_' + str(currentHour),
-                            title='Locations of ' + sentiment.title() + ' Tweets At ' + timeStr)
+                    # for sentiment in sentimentTweets.keys():
+                    #     boston = bm.GreaterBostonScatter(sentimentTweets[sentiment])
+                    #     boston.plotMap(outname=sentiment + '_bombing_scatter_' + str(currentHour),
+                    #         title='Locations of ' + sentiment.title() + ' Tweets At ' + timeStr)
                     # boston = bm.GreaterBostonDensity(kwTweets)
                     # boston.plotMap(outname='bombingDay_density_'+str(currentHour), 
                     #     title='Density of Keyword Tweets At ' + timeStr)
-                    # boston = bm.GreaterBostonScatter(kwTweets)
-                    # boston.plotMap(outname='bombingDay_scatter_'+str(currentHour),
-                    #     title='Locations of Keyword Tweets At ' + timeStr)
-                    # kwTweets.append(tweetData)
+                    boston = bm.GreaterBostonScatter(kwTweets)
+                    boston.plotMap(outname='bombingDay_keyword_'+str(currentHour),
+                        title='Locations of Keyword Tweets At ' + timeStr)
+
+                    boston = bm.GreaterBostonScatter(infoTweets)
+                    boston.plotMap(outname='bombingDay_kw_info_'+str(currentHour),
+                        title='Locations of Keyword Tweets At ' + timeStr)
+
+                    kwTweets.append(tweetData)
+                    if containsHandle(tweetData['tweet_text']):
+                        infoTweets.append(tweetData)
                     plt.close('all')
                     print "Done with " + timeStr
-                    del tweetList
-                    del textList
-                    tweetList = [tweetData]
-                    textList = [tweetData['tweet_text']]
+                    # del tweetList
+                    # del textList
+                    # tweetList = [tweetData]
+                    # textList = [tweetData['tweet_text']]
 
 
 currentHour += 1
 timeStr = getTimeString(currentHour)
-results = clssfr.classify(textList)
-for i in range(0, len(results)):
-    sentimentTweets[cats[results[i]]].append(tweetList[i])
+# results = clssfr.classify(textList)
+# for i in range(0, len(results)):
+#     sentimentTweets[cats[results[i]]].append(tweetList[i])
 
-for sentiment in sentimentTweets.keys():
-    boston = bm.GreaterBostonScatter(sentimentTweets[sentiment])
-    boston.plotMap(outname=sentiment + '_bombing_scatter_' + str(currentHour),
-        title='Locations of ' + sentiment.title() + ' Tweets At ' + timeStr)
+# for sentiment in sentimentTweets.keys():
+#     boston = bm.GreaterBostonScatter(sentimentTweets[sentiment])
+#     boston.plotMap(outname=sentiment + '_bombing_scatter_' + str(currentHour),
+#         title='Locations of ' + sentiment.title() + ' Tweets At ' + timeStr)
 
-    boston = bm.GreaterBostonDensity(sentimentTweets[sentiment])
-    boston.plotMap(outname=sentiment + '_bombing_density_' + str(currentHour),
-        title='Density of ' + sentiment.title() + ' Tweets At ' + timeStr)
-# boston = bm.GreaterBostonScatter(kwTweets)
-# boston.plotMap(outname='bombingDay_scatter_'+str(currentHour),
-#     title='Locations of Keyword Tweets At ' + timeStr)
+#     boston = bm.GreaterBostonDensity(sentimentTweets[sentiment])
+#     boston.plotMap(outname=sentiment + '_bombing_density_' + str(currentHour),
+#         title='Density of ' + sentiment.title() + ' Tweets At ' + timeStr)
+boston = bm.GreaterBostonScatter(kwTweets)
+boston.plotMap(outname='bombingDay_scatter_'+str(currentHour),
+    title='Locations of Keyword Tweets At ' + timeStr)
+
+boston = bm.GreaterBostonScatter(infoTweets)
+boston.plotMap(outname='bombingDay_kw_info_'+str(currentHour),
+    title='Locations of Keyword Tweets At ' + timeStr)
 
 # boston = bm.GreaterBostonDensity(kwTweets)
 # del(kwTweets)
